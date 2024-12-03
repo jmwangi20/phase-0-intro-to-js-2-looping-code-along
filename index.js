81870cff10fdd7b69e734c3ba452c7462f0f8f74
John Kihugo Mwangi <johnmwangi13119@gmail.com>
2024-12-03 13:20:41 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+// #create a function to write thankyou messages for friends
+const birthdayMessages=[]
+const names=["Charlie", "Samip", "Ali"]
+
+function writeCards(names,event){
+    for (let i=0;i<names.length;i++){
+        birthdayMessages.push(`Thankyou ${names[i]} for the ${event} wishes`);
+    }
+    return birthdayMessages;
+}
+
+console.log(writeCards(names,"birthday"));
+
+
+
+function countDown(number){
+    let i=number;
+    while(i>=0){
+      console.log(`${i}`)
+      i--;
+    };
+  }
+console.log(countdownTimer(4));
+
